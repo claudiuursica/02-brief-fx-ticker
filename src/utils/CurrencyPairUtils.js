24f624bf0a764218843e32604c1f61ee3cd676fb
claudiuursica <claudiu.ursica@gmail.com>
2018-03-08 16:49:02 +0000

    Create CurrencyPairUtils.js

---
+++ src/utils/CurrencyPairUtils.js
@@ -0,0 +1,69 @@
+/**
+ * @param {string} currencyPair
+ * @returns {?{currencyFrom: (string), currencyTo: (string)}}
+ */
+export function currencyPairToSymbols(currencyPair)
+{
+	if (isValidCurrencyPair(currencyPair))
+	{
+		const currencyFrom = currencyPair.slice(0, 3);
+		if (!checkLengthSymbol(currencyFrom))
+		{
+			return null;
+		}
+
+		const currencyTo = currencyPair.slice(3);
+		if (!checkLengthSymbol(currencyTo))
+		{
+			return null;
+		}
+
+		return { currencyFrom, currencyTo };
+	}
+
+	return null;
+}
+
+/**
+ * @param {string} currencyPairs comma separated pairs
+ * @returns {Array.<string>}
+ */
+export function currencyPairsToList(currencyPairs)
+{
+	if (isValidCurrencyPairsString(currencyPairs))
+	{
+		const currencyPairsList = trimAllSpaces(currencyPairs).split(',');
+		return currencyPairsList.filter(currencyPair => isValidCurrencyPair);
+	}
+
+	return [];
+}
+
+/**
+ *
+ * @param {string} currencyPair
+ * @returns {boolean}
+ */
+export function isValidCurrencyPair(currencyPair)
+{
+	return isString(currencyPair) && checkLengthCurrencyPair(currencyPair);
+}
+
+/**
+ *
+ * @param {string} currencyPair
+ * @returns {boolean}
+ *
+ * check if at least one pair
+ */
+export function isValidCurrencyPairsString(currencyPair)
+{
+	return isString(currencyPair) && checkLengthCurrencyPair(currencyPair);
+}
+
+
+const isString = currencyPair => typeof currencyPair === 'string';
+const isValidLength = (string, param) => string.length === param;
+const checkLengthCurrencyPair = currencyPair => isValidLength(currencyPair, 6);
+const checkLengthSymbol = currencyPair => isValidLength(currencyPair, 3);
+const trimAllSpaces = (string) => string.replace(/ /g, '');
