133d4bcb77034ca9eabf7f03529ce686ce83fcd2
Claudiu Ursica <claudiu.ursica@gmail.com>
2018-03-08 22:27:59 +0000

    Add multiple pairs bug fixed

--- src/utils/CurrencyPairUtils.js
+++ src/utils/CurrencyPairUtils.js
@@ -33,7 +33,7 @@ export function currencyPairsToList(currencyPairs)
 	if (isValidCurrencyPairsString(currencyPairs))
 	{
 		const currencyPairsList = trimAllSpaces(currencyPairs).split(',');
-		return currencyPairsList.filter(currencyPair => isValidCurrencyPair);
+		return currencyPairsList.filter(currencyPair => isValidCurrencyPair(currencyPair));
 	}
 
 	return [];
@@ -51,19 +51,20 @@ export function isValidCurrencyPair(currencyPair)
 
 /**
  *
- * @param {string} currencyPair
+ * @param {string} currencyPairs
  * @returns {boolean}
  *
  * check if at least one pair
  */
-export function isValidCurrencyPairsString(currencyPair)
+export function isValidCurrencyPairsString(currencyPairs)
 {
-	return isString(currencyPair) && checkLengthCurrencyPair(currencyPair);
+	return isString(currencyPairs) && checkLengthCurrencyPairs(currencyPairs);
 }
-
 
 const isString = currencyPair => typeof currencyPair === 'string';
 const isValidLength = (string, param) => string.length === param;
+const isAtLeastLength = (string, param) => string.length >= param;
 const checkLengthCurrencyPair = currencyPair => isValidLength(currencyPair, 6);
+const checkLengthCurrencyPairs = currencyPair => isAtLeastLength(currencyPair, 6);
 const checkLengthSymbol = currencyPair => isValidLength(currencyPair, 3);
 const trimAllSpaces = (string) => string.replace(/ /g, '');
